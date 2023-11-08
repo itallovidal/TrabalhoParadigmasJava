814b00d168cf664a78192cf2572ea901b9101972
italloVidal <itallovidalll@gmail.com>
2023-11-08 01:59:07 -0300

    add firestore

--- src/main/java/paradigmas/paradigmasCadastramento/ParadigmasCadastramentoApplication.java
+++ src/main/java/paradigmas/paradigmasCadastramento/ParadigmasCadastramentoApplication.java
@@ -1,13 +1,55 @@
 package paradigmas.paradigmasCadastramento;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class ParadigmasCadastramentoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+		ClassLoader classLoader = ParadigmasCadastramentoApplication.class.getClassLoader();
+		InputStream serviceAccount = classLoader.getResourceAsStream("config/serviceAccountKey.json");
+
+		GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+		FirebaseOptions options = FirebaseOptions.builder()
+				.setCredentials(credentials)
+				.build();
+
+		FirebaseApp app;
+
+		if (FirebaseApp.getApps().isEmpty()) {
+			app = FirebaseApp.initializeApp(options, "paradigmasdb");
+		} else {
+			app = FirebaseApp.getApps().get(0);
+		}
+
+		Firestore db = FirestoreClient.getFirestore(app);
+
+		db.collection("users");
+		ApiFuture<QuerySnapshot> query = db.collection("users").get();
+
+		QuerySnapshot querySnapshot = query.get();
+		List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+		for (QueryDocumentSnapshot document : documents) {
+			System.out.println("User: " + document.getData().get("nome"));
+		}
+
 		SpringApplication.run(ParadigmasCadastramentoApplication.class, args);
+
 	}
 
 }
--- src/main/java/paradigmas/paradigmasCadastramento/controller/formController.java
+++ src/main/java/paradigmas/paradigmasCadastramento/controller/formController.java
@@ -1,12 +1,24 @@
 package paradigmas.paradigmasCadastramento.controller;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.database.FirebaseDatabase;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import paradigmas.paradigmasCadastramento.model.*;
+import paradigmas.paradigmasCadastramento.ParadigmasCadastramentoApplication.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 
 
 @Controller
@@ -21,12 +33,12 @@ public class formController {
         }
     };
 
-    @GetMapping("/")
-    public ModelAndView home(){
-        ModelAndView mv = new ModelAndView("index");
-        mv.addObject("cadastros", cadastros);
-        return mv;
-    }
+//    @GetMapping("/")
+//    public ModelAndView home(){
+//        ModelAndView mv = new ModelAndView("index");
+//        mv.addObject("cadastros", cadastros);
+//        return mv;
+//    }
 
     @PostMapping("/")
     public ModelAndView listagem(User user){
@@ -38,5 +50,22 @@ public class formController {
 
         return mv;
     }
+
+    @GetMapping("/")
+    public void teste() throws ExecutionException, InterruptedException {
+
+        Firestore db = FirestoreClient.getFirestore();
+
+        db.collection("users");
+        ApiFuture<QuerySnapshot> query = db.collection("users").get();
+
+        QuerySnapshot querySnapshot = query.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            System.out.println("User: " + document.getId());
+        }
+    }
+
+
 
 }
